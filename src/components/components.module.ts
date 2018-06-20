d73b28b88480f4ba2a1872709ef7b01dfc2735b2
Rodrigo Ovalle <rodrigo.ovalle@dinocloudconsulting.com>
2018-06-20 20:41:26 -0300

    Desafío 1

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HomeContentComponent } from './home-content/home-content';
+@NgModule({
+	declarations: [HomeContentComponent],
+	imports: [],
+	exports: [HomeContentComponent]
+})
+export class ComponentsModule {}
---
+++ src/components/home-content/home-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-content',
+  templateUrl: 'home-content.html'
+})
+export class HomeContentComponent {
+
+  constructor() {}
+
+}
